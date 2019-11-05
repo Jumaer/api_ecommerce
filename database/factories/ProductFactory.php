033ee99a20e9531f4fbe82fa286e32e0ddff169a
Jumaer <jumaerahamedcse.bd.com@gmail.com>
2019-11-04 19:51:15 -0800

    seeding part for Product and Review

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,11 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'price' => $faker->numberBetween(50,10000),
+        'product_detail' => $faker->paragraph,
+        'product_in_stock' => $faker->randomDigit,
+        'product_discount' => $faker->numberBetween(1,70),   
+        'product_of_customer_order' => $faker->randomDigit 
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory(App\Model\Product::class,70)->create();
+        factory(App\Model\Review::class,500)->create();
     }
 }
