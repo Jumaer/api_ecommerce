e5c5e8e2b5ed131a63b6662d23c2004c50ebd1d7
Jumaer <jumaerahamedcse.bd.com@gmail.com>
2019-11-05 19:41:37 -0800

    Modified Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,9 +19,20 @@ class ProductResource extends Resource
                   'name'=>$this->name,
                   'price'=>$this->price,
                   'product_detail'=>$this->product_detail,
-                  'product_in_stock'=>$this->product_in_stock,
+                  'product_in_stock'=>$this->product_in_stock == 0 ? 'Out of Stock' : $this->product_in_stock,
                   'product_discount'=>$this->product_discount,
-                  'product_of_customer_order'=>$this->product_of_customer_order
+                  'totalPrice'=>round(( 1 - ($this->product_discount/100)) * $this->price,2),
+                  'product_of_customer_order'=>$this->product_of_customer_order,
+                  'rating'=>$this->reviews->count()>=1 ? round($this->reviews->sum('star')/$this->reviews->count(),2): 'No rating yet',
+
+
+
+
+                  'href'=> [
+
+                          'reviews' => route('reviews.index',$this->id)
+
+                  ]
 
         ];
     }
